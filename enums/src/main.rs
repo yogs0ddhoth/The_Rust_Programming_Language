86ce7310dea3db9cb2893a1b3e92d76ff3a4a87a
yogs0ddhoth <blin7.webdev@gmail.com>
2022-09-26 19:02:56 -0400

    Chapter 6.1

---
+++ enums/src/main.rs
@@ -0,0 +1,39 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,                       // dataless variant
+    Move { x: i32, y: i32 },    // named fields
+    Write(String),              // single value tuple (String)
+    ChangeColor(i32, i32, i32), // tuple (i32)
+}
+impl Message {
+    fn call(&self) {
+        match &self {
+            Message::Quit => println!("Quit"),
+            Message::Move { x, y } => println!("x: {x}, y: {y}"),
+            Message::Write(str) => println!("{str}"),
+            Message::ChangeColor(x, y, z) => println!("x: {x}, y: {y}, z: {z}"),
+        }
+    }
+}
+
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    dbg!(home);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+    dbg!(loopback);
+
+    let m = Message::Write(String::from("Hiya Chuck."));
+    m.call();
+
+    let some_number = Some(5);
+    
+    if some_number.is_some() {
+        println!("some number: {}", some_number.unwrap());
+    }
+}
