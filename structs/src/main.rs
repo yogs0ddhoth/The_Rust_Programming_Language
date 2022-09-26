040f54e4cbf6dac6e6e201d91de05023e9f82669
yogs0ddhoth <blin7.webdev@gmail.com>
2022-09-26 13:54:56 -0400

    Chapter 5.2 implemented

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -7,12 +7,27 @@ struct User {
 
 struct Point(i32, i32, i32);
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    // create instance of User
-    let mut user1 = build_user(
-        String::from("asdf@mail.com"), 
-        String::from("_")
-    );
+    implement_user();
+
+    // tuple struct implementation
+    let point = Point(0, 0, 0);
+    let (x, y, z) = (point.0, point.1, point.2);
+
+    println!("Coordinates: {x}, {y}, {z}\n");
+
+    implement_rectangle();
+}
+
+fn implement_user() {
+    // create instance of User using the factory function
+    let mut user1 = build_user(String::from("asdf@mail.com"), String::from("_"));
     print_user(&user1);
 
     // update user1
@@ -22,25 +37,16 @@ fn main() {
     // create new User instance
     let user2 = User {
         email: String::from("qwer@mail.com"),
-        ..user1 
-        // user1.username ownership moved to user2.username
-        // user1.active & user1.sign_in_count copied to user2
+        ..user1 // user1.username ownership moved to user2.username
+                // user1.active & user1.sign_in_count copied to user2
 
-        // username: user1.username.clone(), would deeply copy user1.username, if user1 is still needed 
+                // username: user1.username.clone(), would deeply copy user1.username, if user1 is still needed
     };
     print_user(&user2);
     // user1 can no longer be used, as username was moved to user2
     // print_user(&user1); will cause panic
-
-    // tuple struct implementation
-    let point = Point(0,0,0);
-    let (x, y, z) = (point.0, point.1, point.2);
-
-    println!("Coordinates: {x}, {y}, {z}");
-    
 }
-
-// User struct factory
+// User factory
 fn build_user(email: String, username: String) -> User {
     User {
         email,
@@ -51,10 +57,25 @@ fn build_user(email: String, username: String) -> User {
 }
 fn print_user(user: &User) {
     println!(
-        "User: {},\n at {},\n active: {},\n sign in count: {}!\n", 
-        user.username, 
-        user.email,
-        user.active,
-        user.sign_in_count
+        "User: {},\n at {},\n active: {},\n sign in count: {}!\n",
+        user.username, user.email, user.active, user.sign_in_count
     );
+}
+
+fn implement_rectangle() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale), // dbg! helps log expressions
+        height: 50,
+    };
+
+    println!("rect1: {:#?}\n", rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    )
+}
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
