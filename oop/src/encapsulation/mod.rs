1e454fc0fd512b1d85e318b6d9a9b4d147e04721
yogs0ddhoth <blin7.webdev@gmail.com>
2022-11-08 12:28:03 -0500

    17.1

---
+++ oop/src/encapsulation/mod.rs
@@ -0,0 +1,33 @@
+/* mod values are private by default 
+ *
+ * - this allows for encapsulation
+ */
+pub struct AveragedCollection {
+    // private fields
+    list: Vec<i32>,
+    average: f64,
+}
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+    // private method
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
